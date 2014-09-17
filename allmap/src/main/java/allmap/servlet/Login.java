350be55d6b988fb9bc40069de972c19a761a76fa
sawanathtre <sawanathtre@gosoft.co.th>
2014-09-17 15:00:27 +0700

    Add Login Servlet

---
+++ allmap/src/main/java/allmap/servlet/Login.java
@@ -0,0 +1,26 @@
+package allmap.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login") 
+public class Login extends HttpServlet {	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
