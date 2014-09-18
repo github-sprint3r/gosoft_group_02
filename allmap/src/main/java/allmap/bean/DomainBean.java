2e6d35b65a06983b15ff284a8234c3932ddb97d9
thanakritjua <thanakritjua@gosoft.co.th>
2014-09-18 16:47:28 +0700

    Add dopmain bean

---
+++ allmap/src/main/java/allmap/bean/DomainBean.java
@@ -0,0 +1,42 @@
+package allmap.bean;
+
+public class DomainBean {
+	private int domainId;
+	private String domainName;
+	private String domainURL;
+	private String domainCode;
+	private String domainSearchBase;
+	
+	public int getDomainId() {
+		return domainId;
+	}
+	public void setDomainId(int domainId) {
+		this.domainId = domainId;
+	}
+	public String getDomainName() {
+		return domainName;
+	}
+	public void setDomainName(String domainName) {
+		this.domainName = domainName;
+	}
+	public String getDomainURL() {
+		return domainURL;
+	}
+	public void setDomainURL(String domainURL) {
+		this.domainURL = domainURL;
+	}
+	public String getDomainCode() {
+		return domainCode;
+	}
+	public void setDomainCode(String domainCode) {
+		this.domainCode = domainCode;
+	}
+	public String getDomainSearchBase() {
+		return domainSearchBase;
+	}
+	public void setDomainSearchBase(String domainSearchBase) {
+		this.domainSearchBase = domainSearchBase;
+	}
+	
+	
+}
